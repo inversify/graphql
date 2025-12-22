a3c1c3454996427d32df56f894289d16317d0a4e
notaphplover <roberto.pintos.lopez@gmail.com>
2025-12-22 01:23:39 +0100

    feat: update exports to be consistent

--- packages/codegen/src/index.ts
+++ packages/codegen/src/index.ts
@@ -1,4 +1,4 @@
 export { default as generateTsModels } from './graphql/actions/generateTsModels.js';
 export { type GenerateModelsOptions } from './graphql/models/GenerateModelsOptions.js';
 export { default as readSchemas } from './io/calculations/readSchemas.js';
-export { ReadSchemasOptions } from './io/models/ReadSchemasOptions.js';
+export { type ReadSchemasOptions } from './io/models/ReadSchemasOptions.js';
